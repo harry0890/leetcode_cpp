a4a4b50a84244223c14c6af8c58720b24a6243ef
harry0890 <eternity050890@gmail.com>
2019-08-17 15:42:30 +0800

    Update 53_Maximum_Subarray.cpp

--- 53_Maximum_Subarray.cpp
+++ 53_Maximum_Subarray.cpp
@@ -4,13 +4,12 @@
 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        
+    int maxSubArray(vector<int>& nums) {       
         int sum = 0;
         int ans = nums[0];
         
         for(int i=0; i<nums.size(); i++){
-            if(sum >= 0)
+            if(sum >= 0)        // if sum is integer, it's able to add negative value.
                 sum += nums[i];
             else
                 sum = nums[i];
